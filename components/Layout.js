48fec7f76aaa48192b5d5a2421864238a0a52b41
zengtai <zengtai@gmail.com>
2022-01-18 16:25:01 +0800

    修复最低内容高度问题

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,19 @@
 import Head from "next/head";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { SITE_NAME } from "../lib/constants";
+import Container from "./Container";
 
 export default function Layout({ list, children }) {
   // console.log(list);
   return (
-    <>
+    <div className="flex flex-col text-sm bg-yellow-400 text-stone-900/80 min-h-screen">
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+
       <Navbar>{list}</Navbar>
       {children}
       <Footer></Footer>
-    </>
+    </div>
   );
 }
