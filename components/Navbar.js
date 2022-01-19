440819774dcaa278f2fc85b6b83eacffad925f37
zengtai <zengtai@gmail.com>
2022-01-19 20:02:23 +0800

    导航自适应行为

--- components/Navbar.js
+++ components/Navbar.js
@@ -51,7 +51,7 @@ export default function Navbar({ children }) {
         </Link>
         <button
           onClick={toggle}
-          className="flex justify-center items-center w-10 h-10"
+          className="flex justify-center items-center w-10 h-10 md:hidden"
         >
           {isOpen ? (
             <svg
@@ -86,7 +86,13 @@ export default function Navbar({ children }) {
           )}
         </button>
       </div>
-      <div className={isOpen ? `hidden` : `block m-3 relative z-10`}>
+      <div
+        className={
+          isOpen
+            ? `hidden md:block  m-3  z-10 relative`
+            : `block m-3 relative z-10`
+        }
+      >
         <ul className="flex flex-wrap p-2 mt-2 capitalize bg-slate-800/80 rounded-3xl shadow-lg shadow-slate-900/20">
           {categoryNav}
         </ul>
