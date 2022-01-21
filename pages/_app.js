a470e32221bed8c174ba3c8fcdafa8a4e33caa4d
Tai Zeng <zengtai@gmail.com>
2022-01-21 14:49:54 +0800

    添加GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
+import Script from "next/script";
 import { useRouter } from "next/router";
 import NProgress from "nprogress";
 import "../styles/globals.css";
 import "../public/nprogress.css";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -12,8 +14,12 @@ function MyApp({ Component, pageProps }) {
       NProgress.start();
     };
     const handleStop = () => {
+      gtag.pageview(url);
       NProgress.done();
     };
+    // const handleRouteChange = (url) => {
+    //   gtag.pageview(url);
+    // };
 
     router.events.on("routeChangeStart", handleStart);
     router.events.on("routeChangeComplete", handleStop);
@@ -24,9 +30,32 @@ function MyApp({ Component, pageProps }) {
       router.events.off("routeChangeComplete", handleStop);
       router.events.off("routeChangeError", handleStop);
     };
-  }, [router]);
+  }, [router.events]);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
