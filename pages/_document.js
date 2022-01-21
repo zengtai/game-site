544c1482750f9f0ace3188770de1446de0484e34
Tai Zeng <zengtai@gmail.com>
2022-01-21 23:10:09 +0800

    测试Adsense代码

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,8 @@ class MyDocument extends Document {
         <Head>
           <script
             async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9209477879340784`}
+            // src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
             crossOrigin="anonymous"
           />
         </Head>
