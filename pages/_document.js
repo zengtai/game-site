48e53325cbb6e3678cc3fcb8cc945fb8594a62e0
Tai Zeng <zengtai@gmail.com>
2022-01-21 17:29:51 +0800

    测试Adsense代码

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   // static async getInitialProps(ctx) {
@@ -9,7 +10,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <Script
+            strategy="beforeInteractive"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9209477879340784"
+            crossOrigin="anonymous"
+          ></Script>
+        </Head>
         <body>
           <Main />
           <NextScript />
