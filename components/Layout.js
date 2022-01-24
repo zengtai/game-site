66165b4365145f055d1b9dcf2452c9eb2e74dc19
Tai Zeng <zengtai@gmail.com>
2022-01-24 13:16:05 +0800

    修改说明；更换favicon

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,6 @@ export default function Layout({ list, children }) {
     <div className="flex flex-col text-sm bg-gradient-to-br from-blue-800 to-blue-900 text-white/80 min-h-screen">
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="shortcut icon" href="/brand/favicon.svg" sizes="128x128" />
       </Head>
 
       <Navbar>{list}</Navbar>
