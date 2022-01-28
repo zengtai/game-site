f2be60294a1cea332300f5092d46ba969da4fe08
Tai Zeng <zengtai@gmail.com>
2022-01-28 08:29:46 +0800

    设置广告位背景及文字说明

--- pages/game/[slug].js
+++ pages/game/[slug].js
@@ -20,7 +20,7 @@ export default function Games({
   return (
     <>
       <Layout list={categories}>
-        <div className="h-[100px] w-[300px] md:h-[90px] md:w-[728px] lg:w-[970px] mx-auto">
+        <div className="before:content-['Advertisement'] before:flex before:justify-center before:opacity-50 bg-black/5 h-[100px] w-[300px] md:h-[90px] md:w-[728px] lg:w-[970px] mx-auto">
           <AdsenseFixed slot="7173362568" />
         </div>
         <div className="grow p-4 md:p-8 relative z-30">
@@ -48,7 +48,7 @@ export default function Games({
             </div>
           </div>
         </div>
-        <div className="h-[200px] w-[300px] md:h-[90px] md:w-[728px] lg:w-[970px] mx-auto">
+        <div className="before:content-['Advertisement'] before:flex before:justify-center before:opacity-50 bg-black/5 h-[200px] w-[300px] md:h-[90px] md:w-[728px] lg:w-[970px] mx-auto">
           <Adsense slot="7173362568" />
         </div>
       </Layout>
