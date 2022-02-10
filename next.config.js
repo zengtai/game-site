8dc22989ed181c31246722ff27a67be3c80d24cf
Tai Zeng <zengtai@gmail.com>
2022-02-11 00:29:31 +0800

    去掉隐藏未填充广告样式

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ module.exports = {
     disableStaticImages: true,
   },
   reactStrictMode: true,
-  basePath: "/webs/baxiang/baxiang_main_new",
-  // basePath: "/www/channel/baxiang",
+  // basePath: "/webs/baxiang/baxiang_main_new", // 巴象路径
+  // basePath: "/www/channel/baxiang", // 测试用
   distDir: "build",
   trailingSlash: true,
   // assetPrefix: ".",
