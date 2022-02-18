299ddb53322efb1091e3664d98c376e8e3ffd2a7
Tai Zeng <zengtai@gmail.com>
2022-02-18 11:12:37 +0800

    adbee生成设置

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ module.exports = {
   reactStrictMode: true,
   // basePath: "/webs/baxiang/baxiang_main_new", // 巴象路径1
   // basePath: "/webs/baxiang/baxiang_main__new", // 巴象路径2
-  // basePath: "/webs/adbee/main", // adbee路径1
-  basePath: "/webs/adbee/main_", // adbee路径2
+  basePath: "/webs/adbee/main", // adbee路径1
+  /* basePath: "/webs/adbee/main_", // adbee路径2 */
   // basePath: "/www/channel/baxiang", // 测试用
   distDir: "build",
   trailingSlash: true,
