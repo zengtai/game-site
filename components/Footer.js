bb65dd0e661ea856037356eb97cf76d20713c5ff
zengtai <zengtai@gmail.com>
2022-05-30 19:23:13 +0800

    新增导出

--- components/Footer.js
+++ components/Footer.js
@@ -2,16 +2,20 @@ import { SITE_META } from "../lib/constants";
 import Link from "next/link";
 export default function Footer(params) {
   return (
-    <div className="mt-3 text-xs text-center text-yellow-100/50 bg-emerald-700/80">
-      <nav className="group bg-emerald-600/80 p-3 space-x-5 shadow">
+    <div className="mt-3 bg-emerald-700/80 text-center text-xs text-yellow-100/50">
+      <nav className="flex justify-center gap-5 bg-emerald-600/80 p-3 shadow">
         <Link href={`/t/privacy-policy`}>
-          <a title={`Privacy Policy`}>Privacy Policy</a>
+          <a className="xl:hover:text-yellow-100/70" title={`Privacy Policy`}>
+            Privacy Policy
+          </a>
         </Link>
         <Link href={`/t/terms-of-use`}>
-          <a title={`Terms of Use`}>Terms of Use</a>
+          <a className="xl:hover:text-yellow-100/70" title={`Terms of Use`}>
+            Terms of Use
+          </a>
         </Link>
       </nav>
-      <p className="py-7 opacity-90">
+      <p className="py-7 leading-5 opacity-90">
         Copyright &copy; {new Date().getFullYear()} {SITE_META.name}
         <br />
         All Rights Reserved
--- next.config.js
+++ next.config.js
@@ -7,10 +7,15 @@ module.exports = {
   reactStrictMode: true,
   // swcMinify: true,
   // basePath: "/webs/youle", // youle
-  // distDir: "build",
+  // basePath: "/webs/youle_1", // youle_1 2022.05.24
+  // basePath: "/webs/youle_2", // youle_2 2022.05.30
+  distDir: "build",
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
-    return "20220517";
+    // return "20220517";
+    // return "20220524";
+    return "20220530";
   },
   trailingSlash: true,
+  // assetPrefix: `./`,
 };
