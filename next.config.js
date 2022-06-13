8359d2a9921270ee916bae5fe4cab4203aa7f607
zengtai <zengtai@gmail.com>
2022-06-13 14:23:36 +0800

    隐藏导出路径

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ module.exports = {
     disableStaticImages: true,
   },
   reactStrictMode: true,
-  // basePath: "/webs/baxiang/baxiang_main_new", // 巴象路径
-  basePath: "/webs/baxiang/baxiang_main__new", // AdBee路径
+  // basePath: "/webs/baxiang/baxiang_main_new", // 巴象路径1
+  // basePath: "/webs/baxiang/baxiang_main__new", // 巴象路径2
   // basePath: "/www/channel/baxiang", // 测试用
   distDir: "build",
   trailingSlash: true,
