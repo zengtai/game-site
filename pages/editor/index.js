71e1d02dfd729f4aa1fb7178a348c4dae6595b72
zengtai <zengtai@gmail.com>
2022-06-29 10:46:28 +0800

    暂存一下

--- pages/editor/index.js
+++ pages/editor/index.js
@@ -4,6 +4,7 @@ import useCurrentData from "../../data/CurrentData";
 import moment from "moment";
 import { getStars, toSlug, toTitle } from "../../utils/generator";
 import { GAME_PATH, LANDSCAPE_GAMES } from "../../lib/constants";
+import Image from "next/image";
 
 export default function Editor({ data }) {
   function ShowCurrentData() {
@@ -96,45 +97,61 @@ export default function Editor({ data }) {
     return (
       <>
         <div className="my-4">Original Data: {data.gamelist.length}</div>
-        <div className="flex flex-row gap-6">
-          <table className="basis-1/2 table-auto text-sm leading-8">
-            <thead>
-              <tr>
-                <th>
-                  <input type="checkbox" name="" id="" />
-                </th>
-                <th>#</th>
-                <th>appid</th>
-                <th>category</th>
-                <th>creation_date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.gamelist.map((game, index) => (
-                <tr key={game.id}>
-                  <td>
-                    <input type="checkbox" name="" id={game.name} />
-                  </td>
-                  <td>{index + 1}</td>
-                  <td>{game.name}</td>
-                  <td>
-                    {game.category[0] == game.category[0].toUpperCase() &&
-                    game.category == game.category.trim() ? (
-                      game.category
-                    ) : (
-                      <span className="text-red-500">{game.category}</span>
-                    )}
-                  </td>
-                  <td>{moment(new Date(game.time)).format("MMM Do, YYYY")}</td>
+        <div className="flex h-80 flex-row gap-6">
+          <div className="basis-1/2 overflow-auto border-4 border-slate-100">
+            <table className="w-full table-auto text-xs leading-8">
+              <thead>
+                <tr>
+                  <th>
+                    <input type="checkbox" name="" id="" />
+                  </th>
+                  <th>#</th>
+                  <th>icon</th>
+                  <th>appid</th>
+                  <th>category</th>
+                  <th>creation_date</th>
                 </tr>
-              ))}
-              <tr>
-                <td></td>
-              </tr>
-            </tbody>
-          </table>
+              </thead>
+              <tbody className="">
+                {data.gamelist
+                  .sort((a, b) =>
+                    new Date(a.time) < new Date(b.time) ? 1 : -1
+                  )
+                  .map((game, index) => (
+                    <tr key={game.id}>
+                      <td>
+                        <input type="checkbox" name="" id={game.name} />
+                      </td>
+                      <td>{index + 1}</td>
+                      <td className="relative h-16 w-16">
+                        <Image
+                          className="bg-slate-100"
+                          src={game.icon}
+                          alt={game.name}
+                          width={40}
+                          height={40}
+                          layout={`responsive`}
+                        />
+                      </td>
+                      <td>{game.name}</td>
+                      <td>
+                        {game.category[0] == game.category[0].toUpperCase() &&
+                        game.category == game.category.trim() ? (
+                          game.category
+                        ) : (
+                          <span className="text-red-500">{game.category}</span>
+                        )}
+                      </td>
+                      <td>
+                        {moment(new Date(game.time)).format("MMM Do, YYYY")}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
           <textarea
-            className="basis-1/2 select-all bg-slate-50 p-6"
+            className="basis-1/2 select-all border-4 border-slate-100 bg-slate-50 p-6 text-xs"
             name=""
             id=""
             cols="30"
