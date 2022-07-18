16a60097c53661a035445740d4ccd7afc288b089
Tai Zeng <zengtai@gmail.com>
2022-07-19 00:02:16 +0800

    调整图片处理配置

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@ module.exports = {
   images: {
     domains: ["cdn.iwantalipstick.com", "lab.uptapgame.com"],
     formats: ["image/avif", "image/webp"],
-    minimumCacheTTL: 60,
+    // minimumCacheTTL: 60,
+    deviceSizes: [640, 750, 1080, 1200, 1920],
+    imageSizes: [64, 96, 128, 256],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   reactStrictMode: true,
 };
